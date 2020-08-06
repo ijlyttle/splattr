49dc42bb17cf939e2f3338420617d106e5ef817b
Travis CI User <travis@example.org>
2020-08-06 18:47:33 +0000

    Built site for splattr: 0.0.0.9002@e5b083b

---
+++ articles/splattr_files/splattr_ramp-binding-0.0.0.9002/splattr_ramp.js
@@ -0,0 +1,39 @@
+HTMLWidgets.widget({
+
+  name: "splattr_ramp",
+
+  type: "output",
+
+  factory: function(el, width, height) {
+
+    // create our sigma object and bind it to the element
+    var rmp = new Ramp(el.id);
+
+    // insist on the style of the parent
+    el.style.width = "100%";
+    el.style.height = "auto";
+    el.style.lineHeight = 0; // otherwise a few stray pixels appear at the bottom
+
+    return {
+      renderValue: function(x) {
+
+        el.style.background = x.background;
+
+        rmp.colors = x.colors;
+        rmp.n = x.n;
+        rmp.height = x.height;
+        rmp.margin = x.margin;
+        rmp.background = x.background;
+        rmp.redraw();
+      },
+
+      resize: function(width, height) {
+        rmp.resize(width, height);
+        rmp.redraw();
+      },
+
+      // return the object
+      r: rmp
+    };
+  }
+});

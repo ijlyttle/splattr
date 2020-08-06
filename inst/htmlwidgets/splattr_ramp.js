e5b083bf9c8be5997010d004a36b520fa39184d7
Ian Lyttle <ijlyttle@users.noreply.github.com>
2020-08-06 13:43:07 -0500

    enable margin and background for ramp (#8)
    * enable margin and background for ramp
    
    * fix typo

--- inst/htmlwidgets/splattr_ramp.js
+++ inst/htmlwidgets/splattr_ramp.js
@@ -10,13 +10,20 @@ HTMLWidgets.widget({
     var rmp = new Ramp(el.id);
 
     // insist on the style of the parent
-    el.style = "width: 100%; height: auto;";
+    el.style.width = "100%";
+    el.style.height = "auto";
+    el.style.lineHeight = 0; // otherwise a few stray pixels appear at the bottom
 
     return {
       renderValue: function(x) {
+
+        el.style.background = x.background;
+
         rmp.colors = x.colors;
         rmp.n = x.n;
         rmp.height = x.height;
+        rmp.margin = x.margin;
+        rmp.background = x.background;
         rmp.redraw();
       },
 

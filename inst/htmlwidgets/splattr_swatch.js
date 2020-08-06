0c3c7c3cf4f208d34e3ea86a11bad513e0272101
Ian Lyttle <ijlyttle@users.noreply.github.com>
2020-08-06 10:20:04 -0500

    get background working (#6)
    * get background working
    
    * tweak formatting - background set on div
    
    * bump version

--- inst/htmlwidgets/splattr_swatch.js
+++ inst/htmlwidgets/splattr_swatch.js
@@ -10,13 +10,20 @@ HTMLWidgets.widget({
     var swt = new Swatch(el.id);
 
     // insist on the style of the parent
-    el.style = "width: 100%; height: auto;";
+    el.style.width = "100%";
+    el.style.height = "auto";
+    el.style.lineHeight = 0; // otherwise a few stray pixels appear at the bottom
 
     return {
       renderValue: function(x) {
+
+        el.style.background = x.background;
+
         swt.colors = x.colors;
         swt.dx = x.dx;
         swt.height = x.height;
+        swt.margin = x.margin;
+        swt.background = x.background;
         swt.redraw();
       },
 
